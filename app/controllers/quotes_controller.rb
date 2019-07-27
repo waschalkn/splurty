2f1bc88ab6f359e464dc93c22153cb1de257de60
waschalkn <nacodaw93@gmail.com>
2019-07-27 16:55:56 +0000

    Made some style changes to homepagae and modal form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,18 +3,18 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-
-  def create
-    @quote = Quote.create(quote_params)
-    if @quote.invalid?
-      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
-    end
-    redirect_to root_path
+ def create
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
+  redirect_to root_path
+end 
 
-  def about
 
-  end
+
+def about
+end
 
   private
 
